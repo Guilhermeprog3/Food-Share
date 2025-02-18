d86997bfc6273ca7f687e857498870a27d516c1f
guilhermeprog3 <guilhermeriosprog@gmail.com>
2025-02-18 20:10:25 -0300

    feat:adcionando funcionalidade basica

---
+++ foodshare/src/app/(public_pages)/login/page.tsx
@@ -0,0 +1,11 @@
+import { Form_Login } from "@/components/form_login";
+
+const LoginPage = () => {
+    return ( 
+        <div className="h-full justify-center flex items-center">
+            <Form_Login/>
+        </div>
+     );
+}
+ 
+export default LoginPage;
