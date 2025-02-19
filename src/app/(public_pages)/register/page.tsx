558ef685afa037539d2bc9c88860faabadfdbd1d
guilhermeprog3 <guilhermeriosprog@gmail.com>
2025-02-19 11:14:13 -0300

    feat:adcionando pagina de login e de cadastro

--- src/app/(public_pages)/register/page.tsx
+++ src/app/(public_pages)/register/page.tsx
@@ -1,9 +1,16 @@
 import { Form_Register } from "@/components/FormRegister/form_register";
+import Navbar from "@/components/Navbar_login";
 
 const Register_Page = () => {
     return ( 
-        <div className="flex justify-center items-center h-full">
+        <div>
+            <div className="mb-9">
+             <Navbar/>   
+            </div>
+            
+        <div className="h-full justify-center flex items-center">
             <Form_Register/>
+        </div>
         </div>
      );
 }
