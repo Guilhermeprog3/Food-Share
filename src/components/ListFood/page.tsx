47a32578df823d80cecbd97e74cf4a9dd8767932
guilhermeprog3 <guilhermeriosprog@gmail.com>
2025-02-20 19:33:49 -0300

    enchanchement ajustando pagina de alimentos

--- src/components/ListFood/page.tsx
+++ src/components/ListFood/page.tsx
@@ -1,39 +1,41 @@
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
-import { CardDemoList } from "../CardList/page"
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { CardDemoList } from "../CardList/page";
 import {
   Card,
   CardContent,
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
-import { redirect } from "next/navigation"
+} from "@/components/ui/card";
+import { redirect } from "next/navigation";
 
-
-type CardProps = React.ComponentProps<typeof Card>
+type CardProps = React.ComponentProps<typeof Card>;
 
 export function CardDemo({ className, ...props }: CardProps) {
 
-  function handlecreate(){
-    redirect('/aliments_create')
-   }
-   
+  function handlecreate() {
+    redirect('/aliments_create');
+  }
+
   return (
-    <div className="flex justify-center rounded-3xl">
-      <Card className={cn("w-[1000px]", className)} {...props}>
-        <CardHeader>
-          <CardTitle className="text-center text-2xl font-bold mb-4">LISTA DE ALIMENTOS</CardTitle>
-          <Button className="bg-primary w-44 text-primary-foreground rounded-2xl hover:bg-orange-200" onClick={handlecreate}>Cadastrar</Button>
+    <div className="flex justify-center mt-8">
+      <Card className={cn("w-[1000px] p-6 rounded-lg shadow-xl border border-gray-200", className)} {...props}>
+        <CardHeader className="flex justify-between items-center">
+          <CardTitle className="text-center text-2xl font-bold">LISTA DE ALIMENTOS</CardTitle>
+          <Button className="bg-primary text-primary-foreground py-2 px-6 rounded-lg hover:bg-orange-200" onClick={handlecreate}>
+            Cadastrar
+          </Button>
         </CardHeader>
-        <CardContent className="grid gap-4">
-          <div className=" flex items-center space-x-4 rounded-md p-4">
-              <CardDemoList/>
+        <CardContent className="grid gap-6 mt-4">
+          <div className="flex items-center justify-center">
+            <CardDemoList />
           </div>
         </CardContent>
-        <CardFooter>
+        <CardFooter className="flex justify-center mt-4">
+          <p className="text-xs text-gray-500">Gerencie seus alimentos cadastrados</p>
         </CardFooter>
       </Card>
     </div>
-  )
+  );
 }
