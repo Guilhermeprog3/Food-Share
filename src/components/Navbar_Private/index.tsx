6ff5b7605dd05d5bd3965622197cf00ec73d6297
guilhermeprog3 <guilhermeriosprog@gmail.com>
2025-02-21 14:48:29 -0300

    feat:Adcionando navegação da navbar

--- src/components/Navbar_Private/index.tsx
+++ src/components/Navbar_Private/index.tsx
@@ -7,20 +7,30 @@ import { Button } from "../ui/button";
 import Cookies from "js-cookie";
 
 const Navbar_Private = () => {
+
   const handleLogout = () => {
     Cookies.remove("JWT");
     redirect("/");
   };
+  const handlealiments = () => {
+    redirect("/listfood")
+  }
+
+  const handledashboard = () => {
+    redirect("/dashboard")
+  }
 
   return (
-    <div className="bg-card h-13 border-b-2 border-slate-900 p-3">
+    <div className="bg-card h-13 border-b-[0.5px] border-slate-50 p-3">
       <div className="flex content-center gap-60 justify-between">
         <div className="flex justify-start items-center">
-          <img src={logo.src} alt="Food_share-Logo" className="h-[40px]" />
+          <button onClick={handledashboard}>
+            <img src={logo.src} alt="Food_share-Logo" className="h-[40px]" />
+          </button>
           <h1 className="ml-8 text-3xl">FoodsShare</h1>
         </div>
         <div className="flex items-center space-x-4">
-          <Button className="rounded-xl">Alimentos</Button>
+          <Button onClick={handlealiments} className="rounded-xl">Alimentos</Button>
           <Button className="rounded-xl">Agendamentos</Button>
           <Button className="rounded-xl" onClick={handleLogout}>
             Sair
